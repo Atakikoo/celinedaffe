74c01562f35e17b9075ebb40f6aa0632dba99b9a
Atakikoo <atakikoo@gmail.com>
2016-03-17 11:23:57 +0100

    fixed opacity skills

--- js/app.js
+++ js/app.js
@@ -17,18 +17,22 @@ $(function() {
     var scrolled = $window.scrollTop(),
         win_height_padded = $window.height() * 1.1;
 
+
     // Showed...
     $(".revealOnScroll:not(.animated)").each(function () {
       var $this     = $(this),
           offsetTop = $this.offset().top;
 
+
       if (scrolled + win_height_padded > offsetTop) {
         if ($this.data('timeout')) {
           window.setTimeout(function(){
             $this.addClass('animated ' + $this.data('animation'));
+            $this.removeClass('revealOnScrollHide');
           }, parseInt($this.data('timeout'),10));
         } else {
           $this.addClass('animated ' + $this.data('animation'));
+          $this.removeClass('revealOnScrollHide');
         }
       }
     });
@@ -38,6 +42,7 @@ $(function() {
           offsetTop = $this.offset().top;
       if (scrolled + win_height_padded < offsetTop) {
         $(this).removeClass('animated bounceInUp flipInX lightSpeedIn')
+     $this.addClass('revealOnScrollHide');
       }
     });
   }
@@ -47,8 +52,8 @@ $(function() {
 
 
 // add class on hover
-var projectEffect = 'pulse';
-$('.project').hover(
+var projectEffect = 'animated pulse';
+$('.projectImg').hover(
        function(){ $(this).addClass(projectEffect) },
        function(){ $(this).removeClass(projectEffect) }
 )
